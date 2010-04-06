6325aaaeb07c86b82eae6ba23e3be1ec67f09fa6
dnovikov <dnovikov@localhost>
2010-04-06 09:16:32 +0000

    DO NOT DOWNLOAD. DOWNLOAD LATEST STABLE FROM "DOWNLOADS" TAB - The [Nop_NewsLetterSubscriptionLoadAll] stored procedure has been extended and now it checks whether customer is active and not deleted.

--- NopCommerceStore/Documents/Upgrade.sql
+++ NopCommerceStore/Documents/Upgrade.sql
@@ -1019,3 +1019,29 @@ BEGIN
 		[LanguageId] = @LanguageId
 END
 GO
+
+IF EXISTS (
+		SELECT *
+		FROM dbo.sysobjects
+		WHERE id = OBJECT_ID(N'[dbo].[Nop_NewsLetterSubscriptionLoadAll]') AND OBJECTPROPERTY(id,N'IsProcedure') = 1)
+DROP PROCEDURE [dbo].[Nop_NewsLetterSubscriptionLoadAll]
+GO
+CREATE PROCEDURE [dbo].[Nop_NewsLetterSubscriptionLoadAll]
+(
+	@ShowHidden bit = 0
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+	SELECT 
+		nls.* 
+	FROM
+		[Nop_NewsLetterSubscription] nls
+	LEFT OUTER JOIN 
+		Nop_Customer c 
+	ON 
+		nls.Email=c.Email
+	WHERE
+		(nls.Active = 1 AND (c.CustomerID IS NULL OR (c.Active = 1 AND c.Deleted = 0))) OR @ShowHidden = 1
+END
+GO
