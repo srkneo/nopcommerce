57ad4df9763b9fd5bf011a6fc4e47a185f2a261d
AndreyMaz <AndreyMaz@localhost>
2010-05-18 06:50:10 +0000

    DO NOT DOWNLOAD. DOWNLOAD LATEST STABLE FROM "DOWNLOADS" TAB More forum subsription validation (user should be active)

--- NopCommerceStore/Documents/Upgrade.sql
+++ NopCommerceStore/Documents/Upgrade.sql
@@ -8210,7 +8210,6 @@ BEGIN
 END
 GO
 
---price pattern
 IF NOT EXISTS (
 		SELECT 1
 		FROM [dbo].[Nop_Setting]
@@ -8218,5 +8217,79 @@ IF NOT EXISTS (
 BEGIN
 	INSERT [dbo].[Nop_Setting] ([Name], [Value], [Description])
 	VALUES (N'ProductAttribute.PricePattern', N'(?<val>(\d+[\s\,\.]?)+)', N'')
+END
+GO
+
+--forum subsription changes
+IF EXISTS (
+		SELECT *
+		FROM dbo.sysobjects
+		WHERE id = OBJECT_ID(N'[dbo].[Nop_Forums_SubscriptionLoadAll]') AND OBJECTPROPERTY(id,N'IsProcedure') = 1)
+DROP PROCEDURE [dbo].[Nop_Forums_SubscriptionLoadAll]
+GO
+CREATE PROCEDURE [dbo].[Nop_Forums_SubscriptionLoadAll]
+(
+	@UserID				int,
+	@ForumID			int,
+	@TopicID			int,
+	@PageIndex			int = 0, 
+	@PageSize			int = 2147483644,
+	@TotalRecords		int = null OUTPUT
+)
+AS
+BEGIN
+	--paging
+	DECLARE @PageLowerBound int
+	DECLARE @PageUpperBound int
+	DECLARE @RowsToReturn int
+	
+	SET @RowsToReturn = @PageSize * (@PageIndex + 1)	
+	SET @PageLowerBound = @PageSize * @PageIndex
+	SET @PageUpperBound = @PageLowerBound + @PageSize + 1
+	
+	CREATE TABLE #PageIndex 
+	(
+		IndexID int IDENTITY (1, 1) NOT NULL,
+		SubscriptionID int NOT NULL,
+		CreatedOn datetime NOT NULL,
+	)
+
+	INSERT INTO #PageIndex (SubscriptionID, CreatedOn)
+	SELECT DISTINCT
+		fs.SubscriptionID, fs.CreatedOn
+	FROM Nop_Forums_Subscription fs with (NOLOCK)
+	INNER JOIN Nop_Customer c with (NOLOCK) ON fs.UserID=c.CustomerID
+	WHERE   (
+				@UserID IS NULL OR @UserID=0
+				OR (fs.UserID=@UserID)
+			)
+		AND (
+				@ForumID IS NULL OR @ForumID=0
+				OR (fs.ForumID=@ForumID)
+			)
+		AND (
+				@TopicID IS NULL OR @TopicID=0
+				OR (fs.TopicID=@TopicID)
+			)
+		AND (
+				c.Active=1 AND c.Deleted=0
+			)
+	ORDER BY fs.CreatedOn desc, fs.SubscriptionID desc
+
+	SET @TotalRecords = @@rowcount	
+	SET ROWCOUNT @RowsToReturn
+	
+	SELECT  
+		fs.*
+	FROM
+		#PageIndex [pi]
+		INNER JOIN Nop_Forums_Subscription fs on fs.SubscriptionID = [pi].SubscriptionID
+	WHERE
+		[pi].IndexID > @PageLowerBound AND 
+		[pi].IndexID < @PageUpperBound
+	ORDER BY
+		IndexID
+	
+	SET ROWCOUNT 0
 END
 GO
