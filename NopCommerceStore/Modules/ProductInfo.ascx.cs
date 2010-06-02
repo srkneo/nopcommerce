88f2d205b891a300ae9d31fc283b90c2bad70299
AndreyMaz <AndreyMaz@localhost>
2010-06-02 11:05:05 +0000

    Added nopCommerce 1.70 branch

---
+++ NopCommerceStore/Modules/ProductInfo.ascx.cs
@@ -0,0 +1,98 @@
+//------------------------------------------------------------------------------
+// The contents of this file are subject to the nopCommerce Public License Version 1.0 ("License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at  http://www.nopCommerce.com/License.aspx. 
+// 
+// Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. 
+// See the License for the specific language governing rights and limitations under the License.
+// 
+// The Original Code is nopCommerce.
+// The Initial Developer of the Original Code is NopSolutions.
+// All Rights Reserved.
+// 
+// Contributor(s): _______. 
+//------------------------------------------------------------------------------
+
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Text;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using NopSolutions.NopCommerce.BusinessLogic;
+using NopSolutions.NopCommerce.BusinessLogic.Media;
+using NopSolutions.NopCommerce.BusinessLogic.Products;
+using NopSolutions.NopCommerce.Common.Utils;
+using NopSolutions.NopCommerce.BusinessLogic.Configuration.Settings;
+
+namespace NopSolutions.NopCommerce.Web.Modules
+{
+    public partial class ProductInfoControl : BaseNopUserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+                BindData();
+        }
+
+        protected void BindData()
+        {
+            var product = ProductManager.GetProductById(this.ProductId);
+            if (product != null)
+            {
+                lProductName.Text = Server.HtmlEncode(product.Name);
+                lShortDescription.Text = product.ShortDescription;
+                lFullDescription.Text = product.FullDescription;
+
+                var productPictures = product.ProductPictures;
+                if (productPictures.Count > 1)
+                {
+                    defaultImage.ImageUrl = PictureManager.GetPictureUrl(productPictures[0].PictureId, SettingManager.GetSettingValueInteger("Media.Product.DetailImageSize", 300));
+                    defaultImage.ToolTip = String.Format(GetLocaleResourceString("Media.Product.ImageAlternateTextFormat"), product.Name);
+                    defaultImage.AlternateText = String.Format(GetLocaleResourceString("Media.Product.ImageAlternateTextFormat"), product.Name);
+                    lvProductPictures.DataSource = productPictures;
+                    lvProductPictures.DataBind();
+                }
+                else if (productPictures.Count == 1)
+                {
+                    defaultImage.ImageUrl = PictureManager.GetPictureUrl(productPictures[0].PictureId, SettingManager.GetSettingValueInteger("Media.Product.DetailImageSize", 300));
+                    defaultImage.ToolTip = String.Format(GetLocaleResourceString("Media.Product.ImageAlternateTextFormat"), product.Name);
+                    defaultImage.AlternateText = String.Format(GetLocaleResourceString("Media.Product.ImageAlternateTextFormat"), product.Name);
+                    lvProductPictures.Visible = false;
+                }
+                else
+                {
+                    defaultImage.ImageUrl = PictureManager.GetDefaultPictureUrl(SettingManager.GetSettingValueInteger("Media.Product.DetailImageSize", 300));
+                    defaultImage.ToolTip = String.Format(GetLocaleResourceString("Media.Product.ImageAlternateTextFormat"), product.Name);
+                    defaultImage.AlternateText = String.Format(GetLocaleResourceString("Media.Product.ImageAlternateTextFormat"), product.Name);
+                    lvProductPictures.Visible = false;
+                }
+            }
+            else
+                this.Visible = false;
+        }
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            BindJQuery();
+
+            string slimBox = CommonHelper.GetStoreLocation() + "Scripts/slimbox2.js";
+            Page.ClientScript.RegisterClientScriptInclude(slimBox, slimBox);
+
+            base.OnPreRender(e);
+        }
+
+        public int ProductId
+        {
+            get
+            {
+                return CommonHelper.QueryStringInt("ProductId");
+            }
+        }
+    }
+}
