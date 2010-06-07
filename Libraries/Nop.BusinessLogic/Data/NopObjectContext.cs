0a6e0076d38d88750873764d38deba54702ed00f
AndreyMaz <AndreyMaz@localhost>
2010-06-07 08:07:14 +0000

    DO NOT DOWNLOAD. DOWNLOAD LATEST STABLE FROM "DOWNLOADS" TAB - Further converting changes (VS 2010) - Added EF NopObjectContext (languages)

---
+++ Libraries/Nop.BusinessLogic/Data/NopObjectContext.cs
@@ -0,0 +1,59 @@
+//------------------------------------------------------------------------------
+// The contents of this file are subject to the nopCommerce Public License Version 1.0 ("License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at  http://www.nopCommerce.com/License.aspx. 
+// 
+// Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. 
+// See the License for the specific language governing rights and limitations under the License.
+// 
+// The Original Code is nopCommerce.
+// The Initial Developer of the Original Code is NopSolutions.
+// All Rights Reserved.
+// 
+// Contributor(s): _______. 
+//------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Data.Objects;
+using System.Text;
+using NopSolutions.NopCommerce.BusinessLogic.Directory;
+
+
+namespace NopSolutions.NopCommerce.BusinessLogic.Data
+{
+    /// <summary>
+    /// Represents a nopCommerce object context
+    /// </summary>
+    public partial class NopObjectContext : ObjectContext
+    {
+        #region Ctor
+        /// <summary>
+        /// Creates a new instance of the Address class
+        /// </summary>
+        public NopObjectContext()
+            : base("name=NopEntities", "NopEntities")
+        {
+            this.ContextOptions.LazyLoadingEnabled = true;
+
+        }
+        #endregion
+
+        #region Properties
+
+        public ObjectSet<Language> Languages
+        {
+            get
+            {
+                if ((_languages == null))
+                {
+                    _languages = CreateObjectSet<Language>();
+                }
+                return _languages;
+            }
+        }
+        private ObjectSet<Language> _languages;
+
+        #endregion 
+    }
+
+}
