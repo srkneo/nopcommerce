ebdce29684d914e090e2b583d06b73503e3bbf35
Ben Foster <Ben Foster@localhost>
2010-12-14 18:42:43 +0000

    Added IDbContext (removes dependency on NopObjectContext) Moved mapping configurations from Core assembly. These belong in data assembly Minor refactoring

---
+++ src/Libraries/Nop.Data/NopObjectContext.cs
@@ -0,0 +1,64 @@
+//------------------------------------------------------------------------------
+// The contents of this file are subject to the nopCommerce Public License Version 1.0 ("License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at  http://www.nopCommerce.com/License.aspx. 
+// 
+// Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. 
+// See the License for the specific language governing rights and limitations under the License.
+// 
+// The Original Code is nopCommerce.
+// The Initial Developer of the Original Code is NopSolutions.
+// All Rights Reserved.
+// 
+// Contributor(s): _______. 
+//------------------------------------------------------------------------------
+
+using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Reflection;
+using Nop.Core.Domain;
+using Nop.Core;
+using Nop.Data.Mapping;
+
+namespace Nop.Data
+{
+    /// <summary>
+    /// Object context
+    /// </summary>
+    public class NopObjectContext : DbContext, IDbContext
+    {
+        public NopObjectContext()
+            : base("name=NopSqlConnection")
+        {
+
+        }
+
+        public DbSet<Language> Languages { get; set; }
+
+        public DbSet<Setting> Settings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //dynamically load all configuration
+            System.Type configType = typeof(LanguageMap);   //any of your configuration classes here
+            var typesToRegister = Assembly.GetAssembly(configType).GetTypes()
+            .Where(type => type.Namespace != null && type.Namespace.Equals(configType.Namespace))
+            .Where(type => type.BaseType.IsGenericType && type.BaseType.GetGenericTypeDefinition() == typeof(EntityTypeConfiguration<>));
+            foreach (var type in typesToRegister)
+            {
+                dynamic configurationInstance = Activator.CreateInstance(type);
+                modelBuilder.Configurations.Add(configurationInstance);
+            }
+            //...or do it manually below. For example,
+            //modelBuilder.Configurations.Add(new LanguageMap());
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public new IDbSet<TEntity> Set<TEntity>() where TEntity : BaseEntity  {
+            return base.Set<TEntity>();
+        }
+    }
+}
