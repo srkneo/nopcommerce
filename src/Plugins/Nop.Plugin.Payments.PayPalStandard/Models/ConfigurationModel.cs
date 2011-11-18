399a2b053274022c50fa3ac8633f07fba4d85a6d
nicolas <nicolas@localhost>
2011-11-18 10:59:28 +0100

    Allow setting picture size in the view. In many case, we need to have different image size in BestSellers, or AlsoPuchased etc. This update allow designer to set image size like this : @Html.Action("ProductsAlsoPurchased", "Catalog", new { productId = Model.Id, productThumbPictureSize = 100 }) The new parameter is optional, if you do not use it, default value specified in media settings is used instead.

---
+++ src/Plugins/Nop.Plugin.Payments.PayPalStandard/Models/ConfigurationModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Web.Mvc;
+using Nop.Web.Framework.Mvc;
+
+namespace Nop.Plugin.Payments.PayPalStandard.Models
+{
+    public class ConfigurationModel : BaseNopModel
+    {
+        [DisplayName("Use Sandbox")]
+        public bool UseSandbox { get; set; }
+
+        [DisplayName("Business Email")]
+        public string BusinessEmail { get; set; }
+
+        [DisplayName("PDT Identity Token")]
+        public string PdtToken { get; set; }
+
+        [DisplayName("PDT. Validate order total")]
+        public bool PdtValidateOrderTotal { get; set; }
+
+        [DisplayName("Additional fee")]
+        public decimal AdditionalFee { get; set; }
+
+        [DisplayName("Pass product names and order totals to PayPal")]
+        public bool PassProductNamesAndTotals { get; set; }
+
+        [DisplayName("Enable IPN (Instant Payment Notification)")]
+        public bool EnableIpn { get; set; }
+
+        [DisplayName("Leave blanck to use the default IPN handler url. Orelse you could choose another url for example to use a different domain name (http://payment.myStore.com/Plugins/PaymentPayPalStandard/IPNHandler) or to log IPN on another system.")]
+        public string IpnUrl { get; set; }
+    }
+}
---
+++ src/Presentation/Nop.Web/Models/Checkout/CheckoutBillingAddressModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Nop.Web.Framework.Mvc;
+using Nop.Web.Models.Common;
+
+namespace Nop.Web.Models.Checkout
+{
+    public class CheckoutBillingAddressModel : BaseNopModel
+    {
+        public CheckoutBillingAddressModel()
+        {
+            ExistingAddresses = new List<AddressModel>();
+            NewAddress = new AddressModel();
+        }
+
+        public IList<AddressModel> ExistingAddresses { get; set; }
+
+        public AddressModel NewAddress { get; set; }
+
+        public bool NewAddressPreselected { get; set; }
+    }
+}
