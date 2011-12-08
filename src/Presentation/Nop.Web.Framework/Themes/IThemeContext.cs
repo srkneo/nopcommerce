391eba15f97ad014cef089c957701f42532df1d1
andreymaz <andreymaz@localhost>
2011-12-08 09:50:03 +0400

    Mobile version. Added 'Topic' page

---
+++ src/Presentation/Nop.Web.Framework/Themes/IThemeContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Nop.Web.Framework.Themes
+{
+    /// <summary>
+    /// Work context
+    /// </summary>
+    public interface IThemeContext
+    {
+        /// <summary>
+        /// Get or set current theme for desktops (e.g. darkOrange)
+        /// </summary>
+        string WorkingDesktopTheme { get; set; }
+    }
+}
