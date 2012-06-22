3f5cd8ca7dde44d4602d8f98b05e85663d45873b
Bradley Meine <brad_meine@hotmail.com>
2012-06-22 10:57:54 -0500

    Added index creation script for MySql

---
+++ src/Presentation/Nop.Web/Models/Catalog/ManufacturerNavigationModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Nop.Web.Framework.Mvc;
+
+namespace Nop.Web.Models.Catalog
+{
+    public class ManufacturerNavigationModel : BaseNopModel
+    {
+        public ManufacturerNavigationModel()
+        {
+            this.Manufacturers = new List<ManufacturerBriefInfoModel>();
+        }
+
+        public IList<ManufacturerBriefInfoModel> Manufacturers { get; set; }
+
+        public int TotalManufacturers { get; set; }
+    }
+
+    public class ManufacturerBriefInfoModel : BaseNopEntityModel
+    {
+        public string Name { get; set; }
+
+        public string SeName { get; set; }
+        
+        public bool IsActive { get; set; }
+    }
+}
