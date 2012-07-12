d9b476949b4bf0d72ba6b130f88dcd539a6d7226
andreymaz <andreymaz@localhost>
2012-07-12 08:48:48 +0400

    ITopicService registration was listed twice in DependencyRegistrar class.

---
+++ src/Libraries/Nop.Core/Events/EventPublisherExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Nop.Core.Domain.Messages;
+namespace Nop.Core.Events
+{
+    public static class EventPublisherExtensions
+    {
+        public static void EntityInserted<T>(this IEventPublisher eventPublisher, T entity) where T : BaseEntity
+        {
+            eventPublisher.Publish(new EntityInserted<T>(entity));
+        }
+
+        public static void EntityUpdated<T>(this IEventPublisher eventPublisher, T entity) where T : BaseEntity
+        {
+            eventPublisher.Publish(new EntityUpdated<T>(entity));
+        }
+
+        public static void EntityDeleted<T>(this IEventPublisher eventPublisher, T entity) where T : BaseEntity
+        {
+            eventPublisher.Publish(new EntityDeleted<T>(entity));
+        }
+        
+        public static void EntityTokensAdded<T, U>(this IEventPublisher eventPublisher, T entity, System.Collections.Generic.IList<U> tokens) where T : BaseEntity
+        {
+            eventPublisher.Publish(new EntityTokensAdded<T, U>(entity, tokens));
+        }
+
+        public static void MessageTokensAdded<U>(this IEventPublisher eventPublisher, MessageTemplate message, System.Collections.Generic.IList<U> tokens)
+        {
+            eventPublisher.Publish(new MessageTokensAdded<U>(message, tokens));
+        }
+    }
+}
